f7e9e55b9ec53e90ea11299f2282855737072f6d
Otto Enoc <ottoenoc@gmail.com>
2026-03-31 00:03:55 -0300

    Update processamento.cpp

--- src/processamento.cpp
+++ src/processamento.cpp
@@ -37,7 +37,7 @@ data.media = (float)(soma / total);
 
 
  // 3. Cálculo da Variância e Desvio Padrão 
-    
+    //variancia em zero
     double variancia = 0;
     for(int i=0; i<256; i++) {
         if(data.histograma[i] > data.maxHist) data.maxHist = data.histograma[i];
@@ -48,4 +48,38 @@ data.media = (float)(soma / total);
     std::string brilho = (data.media < 85) ? "Escura" : (data.media > 170) ? "Clara" : "Media";
     std::string contraste = (data.desvio < 30) ? "Baixo" : (data.desvio > 70) ? "Alto" : "Medio";
 
+    // EXIBIÇÃO NO TERMINAL 
+    std::cout << "\n========================================" << std::endl;
+    std::cout << " ANALISE DA IMAGEM ATUAL " << std::endl;
+    std::cout << "========================================" << std::endl;
+    //media
+    std::cout << "Media de Intensidade: " << data.media << " -> " << brilho << std::endl;
+    //desvio
+    std::cout << "Desvio Padrao:        " << data.desvio << " -> Contraste " << contraste << std::endl;
+    std::cout << "========================================\n" << std::endl;
+}
+
+SDL_Surface* criarEqualizada(SDL_Surface* original, uint32_t* hist) {
+    SDL_Surface* surf = SDL_DuplicateSurface(original);
+    //total de pixel
+    int total = surf->w * surf->h;
+    //veto CDF
+    float cdf[256], somaAcumulada = 0;
     
+    // CDF com as frequências normalizadas do histograma
+    for(int i=0; i<256; i++) {
+        somaAcumulada += (float)hist[i] / total;
+        cdf[i] = somaAcumulada;
+    }
+
+    //perciorre a nova imagem 
+    for (int y = 0; y < surf->h; y++) {
+        for (int x = 0; x < surf->w; x++) {
+            uint8_t r, g, b, a;
+            SDL_ReadSurfacePixel(surf, x, y, &r, &g, &b, &a);
+            uint8_t novoTom = (uint8_t)(cdf[r] * 255);
+            SDL_WriteSurfacePixel(surf, x, y, novoTom, novoTom, novoTom, a);
+        }
+    }
+    return surf;
+}
